4aff6a69256fe05c62da30a0b300e70c3d1cd534
Jared Lee <jared@Jareds-iMac.local>
2020-11-03 14:51:18 -0800

    Adds user feedback to form submission.

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,53 @@
+var RequestOK = false;
 
+var FormElementsInputs = ["contact__input--name", "contact__input--phone",
+    "contact__input--email", "contact__input--details"];
 
 window.addEventListener("load", (event) => {
     $("form").on("submit", function (event) {
         event.preventDefault();
-        
+        RequestOK = false;
+
         $.ajax({
             type: "POST",
             url: "https://formspree.io/f/xqkgbdoj",
             dataType: "json",
             data: $(this).serialize(),
             success: function() {
-                alert("Success!");
-            }
+                RequestOK = true;
+            },
         });
 
-        //TODO: Clear input content & change button text to "Submitted!"
-        //      When user focuses any input element, revert submit button.
-
+        provideUserFeedback();
     });
+
+    FormElementsInputs.forEach(element => {
+        $('.' + element).focus(function () {
+            if (RequestOK) {
+                clearFormElementsInputs();
+                $("#contact-submit-button").val("Submit");
+                RequestOK = false; // User is possibly trying to submit another form.
+            }
+        });
+    });
+
 });
+
+var provideUserFeedback = function () {
+    if (RequestOK) {
+        $("#contact-submit-button").val("Submitted!");
+    }
+
+    else {
+        alert("We're sorry, there was a problem with your submission." +
+        " Please check your internet connection and try again.");
+    }
+}
+
+var clearFormElementsInputs = function () {
+    FormElementsInputs.forEach(element => {
+        $('.' + element).val('');
+    });
+}
 
 
